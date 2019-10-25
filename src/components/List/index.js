e82eef8354b9a3fac688a46fe4b49a015a7455ed
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-25 14:19:01 -0300

    list component created

---
+++ src/components/List/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class List extends React.Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    Chifre de unicórnio
+                </li>
+                <li>
+                    Frauda dourada
+                </li>
+                <li>
+                    carburador de celta
+                </li>
+            </ul>
+        )
+    }
+}
