8e43d844110e2f21524e873254b1c7cf7a56c7f4
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-25 14:21:33 -0300

    list component config

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,17 +3,23 @@ import React from 'react'
 export default class List extends React.Component {
     render() {
         return (
-            <ul>
-                <li>
-                    Chifre de unicórnio
-                </li>
-                <li>
-                    Frauda dourada
-                </li>
-                <li>
-                    carburador de celta
-                </li>
-            </ul>
+            <div>
+
+                <ul>
+                    <li>
+                        Chifre de unicórnio
+                    </li>
+                    <li>
+                        Frauda dourada
+                    </li>
+                    <li>
+                        carburador de celta
+                    </li>
+                </ul>
+                
+                <input type="text" />
+
+            </div>
         )
     }
 }
