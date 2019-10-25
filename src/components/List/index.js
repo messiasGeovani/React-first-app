1230aba04af9fd7b5dda9e81d8ab1c73c4b07a12
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-25 16:14:16 -0300

    list component config

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,23 +1,63 @@
 import React from 'react'
 
 export default class List extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            list: [],
+            updatedList: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleInputSubmit = this.handleInputSubmit.bind(this)
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            updatedList: e.target.value,
+        })
+        console.log(e.target.value)
+    }
+
+    handleInputSubmit(e) {
+        if (this.state.updatedList !== '') {
+            this.setState({
+                list: [
+                    ...this.state.list,
+                    this.state.updatedList
+                ],
+                updatedList: ''
+            })
+
+            console.log(this.state.updatedList)
+        }
+        
+        e.preventDefault()
+    }
+
     render() {
-        return (
+
+        return (            
             <div>
 
                 <ul>
-                    <li>
-                        Chifre de unicórnio
-                    </li>
-                    <li>
-                        Frauda dourada
-                    </li>
-                    <li>
-                        carburador de celta
-                    </li>
+                    {
+                        this.state.list.map((messias, index) => (
+                            <li key={index}>
+                                {messias}
+                            </li>
+                        ))
+                    }
                 </ul>
-                
-                <input type="text" />
+                    
+                <form onSubmit={this.handleInputSubmit}>
+                    
+                    <input type="text" onChange={this.handleInputChange} value={this.state.updatedList} />
+
+                    <input type="submit" value="Enviar" />
+                </form>
 
             </div>
         )
